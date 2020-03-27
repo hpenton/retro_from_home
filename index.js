391c63daeb0ef49661ef7da3109b5acdbc01e02c
Henry Penton <hpenton@worth.systems>
2020-03-27 08:13:15 +0000

    adding dot functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
+const hash = require("object-hash");
 
 let messages = [];
 
@@ -13,12 +14,19 @@ io.on("connection", function(socket) {
   io.emit("chat message", messages);
 
   socket.on("chat message", function(msg) {
-    messages.push(msg);
-    console.log(messages);
+    const messageKey = hash(msg);
+    let newMessage = { name: messageKey, message: msg, dots: 0 };
+
+    messages.push(newMessage);
+
     io.emit("chat message", messages);
   });
   socket.on("reset", () => {
     messages = [];
+    io.emit("chat message", messages);
+  });
+  socket.on("dot", dotKey => {
+    messages.find(el => el.name === dotKey).dots++;
     io.emit("chat message", messages);
   });
 });
